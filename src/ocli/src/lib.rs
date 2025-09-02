5f4f35a5ff2e94bde34ddb1bf2b92b99735e6680
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2025-09-02 13:54:10 +0200

    add config mechanism

--- src/ocli/src/lib.rs
+++ src/ocli/src/lib.rs
@@ -1,4 +1,6 @@
-use anyhow::Result;
+use std::path::PathBuf;
+
+use anyhow::{Context, Result};
 use openidconnect::core::{
     CoreAuthDisplay, CoreClaimName, CoreClaimType, CoreClient, CoreClientAuthMethod,
     CoreDeviceAuthorizationResponse, CoreGrantType, CoreJsonWebKey,
@@ -7,11 +9,13 @@ use openidconnect::core::{
 };
 use openidconnect::{
     AdditionalProviderMetadata, ClientId, DeviceAuthorizationUrl, IssuerUrl, ProviderMetadata,
-    Scope, TokenResponse,
+    Scope,
 };
 use openidconnect::{OAuth2TokenResponse, reqwest};
 
+use regex::Regex;
 use serde::{Deserialize, Serialize};
+use url::Url;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 struct DeviceEndpointProviderMetadata {
@@ -33,6 +37,7 @@ type DeviceProviderMetadata = ProviderMetadata<
     CoreSubjectIdentifierType,
 >;
 
+#[derive(Clone)]
 pub struct OIDCTokenset {
     access_token: String,
     refresh_token: Option<String>,
@@ -47,7 +52,17 @@ impl OIDCTokenset {
     }
 }
 
-pub fn device_code_flow(url: String, client_id: String) -> Result<OIDCTokenset> {
+pub struct DeviceCodeData {
+    pub verify_url_full: String,
+    pub verify_url: String,
+    pub device_code: String,
+    code_response: CoreDeviceAuthorizationResponse,
+    provider_metadata: DeviceProviderMetadata,
+    client_id: String,
+    device_url: DeviceAuthorizationUrl,
+}
+
+pub fn start_device_code_flow(url: String, client_id: String) -> Result<DeviceCodeData> {
     let http_client = reqwest::blocking::ClientBuilder::new()
         .redirect(reqwest::redirect::Policy::none())
         .build()
@@ -57,10 +72,13 @@ pub fn device_code_flow(url: String, client_id: String) -> Result<OIDCTokenset> 
         .additional_metadata()
         .device_authorization_endpoint
         .clone();
-    let client =
-        CoreClient::from_provider_metadata(provider_metadata, ClientId::new(client_id), None)
-            .set_device_authorization_url(device_url)
-            .set_auth_type(openidconnect::AuthType::RequestBody);
+    let client = CoreClient::from_provider_metadata(
+        provider_metadata.clone(),
+        ClientId::new(client_id.clone()),
+        None,
+    )
+    .set_device_authorization_url(device_url.clone())
+    .set_auth_type(openidconnect::AuthType::RequestBody);
     let details: CoreDeviceAuthorizationResponse = client
         .exchange_device_code()
         .add_scope(Scope::new("profile".to_string()))
@@ -69,21 +87,165 @@ pub fn device_code_flow(url: String, client_id: String) -> Result<OIDCTokenset> 
         .verification_uri_complete()
         .map(|u| u.secret().to_owned())
         .expect("couldn't construct the full verification url");
-    println!(
-        "Please visit {} and authorize this application.",
-        verify_url
-    );
-    open::that(verify_url)?;
+    Ok(DeviceCodeData {
+        verify_url_full: verify_url,
+        verify_url: details.clone().verification_uri().to_string(),
+        device_code: details.device_code().clone().into_secret(),
+        code_response: details,
+        provider_metadata,
+        client_id,
+        device_url,
+    })
+}
 
-    let token = client.exchange_device_access_token(&details)?.request(
-        &http_client,
-        std::thread::sleep,
+pub fn finish_device_code_flow(data: DeviceCodeData) -> Result<OIDCTokenset> {
+    let http_client = reqwest::blocking::ClientBuilder::new()
+        .redirect(reqwest::redirect::Policy::none())
+        .build()
+        .expect("Client should build");
+    let client = CoreClient::from_provider_metadata(
+        data.provider_metadata,
+        ClientId::new(data.client_id),
         None,
-    )?;
+    )
+    .set_device_authorization_url(data.device_url)
+    .set_auth_type(openidconnect::AuthType::RequestBody);
+    let token = client
+        .exchange_device_access_token(&data.code_response)?
+        .request(&http_client, std::thread::sleep, None)?;
     let token = OIDCTokenset {
         access_token: token.access_token().secret().to_owned(),
         refresh_token: token.refresh_token().map(|t| t.secret().to_owned()),
     };
 
     Ok(token)
+}
+
+#[derive(Deserialize)]
+pub struct Config {
+    pub name: String,
+    pub client_id: String,
+    pub url: String,
+    pub rules: Vec<Rule>,
+}
+impl Config {
+    pub fn affected_paths(&self) -> Result<Vec<PathBuf>> {
+        self.rules.iter().map(|r| r.resolved_path()).collect()
+    }
+    pub fn download(url: String) -> Result<Config> {
+        let config: Config = if url.starts_with("file://") {
+            let url = Url::parse(url.as_str())?;
+            let path = url
+                .to_file_path()
+                .expect("couldn't convert local uri to file path");
+            let content = std::fs::read(path)?;
+            toml::from_slice(&content)?
+        } else {
+            let response = reqwest::blocking::get(url)?;
+            let content = response.bytes()?;
+            toml::from_slice(&content)?
+        };
+        Ok(config)
+    }
+
+    pub fn apply(&self, tokenset: OIDCTokenset) -> Result<(Vec<&str>, Vec<&str>)> {
+        let mut skipped = Vec::new();
+        let mut applied = Vec::new();
+        for rule in self.rules.iter() {
+            if rule.render(tokenset.clone())? {
+                applied.push(rule.name.as_str());
+            } else {
+                skipped.push(rule.name.as_str());
+            }
+        }
+        Ok((applied, skipped))
+    }
+}
+
+#[derive(Deserialize)]
+pub struct Rule {
+    pub name: String,
+    pub path: String,
+    pub patterns: Vec<String>,
+    pub new_file_template: Option<String>,
+}
+
+impl Rule {
+    pub fn resolved_path(&self) -> Result<PathBuf> {
+        let path = shellexpand::full(&self.path)?;
+        Ok(PathBuf::from(&path.to_string()))
+    }
+
+    fn regex_patterns(&self) -> Vec<Regex> {
+        self.patterns
+            .iter()
+            .map(|p| Regex::new(p).unwrap_or_else(|_| panic!("couldn't parse pattern {}", p)))
+            .collect()
+    }
+
+    pub fn render(&self, tokenset: OIDCTokenset) -> Result<bool> {
+        let path = self.resolved_path()?;
+        let mut matched = false;
+        if path.exists() {
+            // update file
+            matched = self.patch(tokenset)?;
+        } else if self.new_file_template.is_some() {
+            // create file
+            self.new_from_template(tokenset)?;
+            matched = true;
+        }
+
+        Ok(matched)
+    }
+    fn patch(&self, tokenset: OIDCTokenset) -> Result<bool> {
+        let path = self.resolved_path()?;
+        let mut content = std::fs::read_to_string(path.clone())?;
+        let mut matched = false;
+        for pattern in self.regex_patterns() {
+            let mut caps = pattern.captures_iter(content.as_str()).peekable();
+            if caps.peek().is_none() {
+                continue;
+            }
+            let mut new = String::with_capacity(content.len());
+            let mut last_match = 0;
+
+            for cap in caps {
+                if let Some(m) = cap.name("access_token") {
+                    new.push_str(&content[last_match..m.start()]);
+                    new.push_str(tokenset.access_token());
+                    last_match = m.end();
+                    matched = true;
+                }
+                if let Some(m) = cap.name("refresh_token") {
+                    new.push_str(&content[last_match..m.start()]);
+                    new.push_str(tokenset.refresh_token().expect("refresh token not found"));
+                    last_match = m.end();
+                    matched = true;
+                }
+            }
+            content = new;
+        }
+        std::fs::write(path, content)?;
+
+        Ok(matched)
+    }
+
+    fn new_from_template(&self, tokenset: OIDCTokenset) -> Result<()> {
+        let mut tera = tera::Tera::default();
+        tera.add_raw_template(
+            self.name.as_str(),
+            self.new_file_template
+                .clone()
+                .context("new_file_template is empty")?
+                .as_str(),
+        )?;
+        let mut context = tera::Context::new();
+        context.insert("access_token", tokenset.access_token());
+        if let Some(refresh_token) = tokenset.refresh_token() {
+            context.insert("refresh_token", refresh_token);
+        }
+        let rendered = tera.render(self.name.as_str(), &context)?;
+        std::fs::write(self.resolved_path()?, rendered)?;
+        Ok(())
+    }
 }
--- src/ocli/src/main.rs
+++ src/ocli/src/main.rs
@@ -1,16 +1,126 @@
-use clap::Parser;
-use ocli::device_code_flow;
+use anyhow::Result;
+use clap::{Args, Parser, Subcommand};
+use ocli::{Config, finish_device_code_flow, start_device_code_flow};
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about=None)]
-struct Args {
-    #[arg(short, long)]
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+#[derive(Subcommand)]
+enum Commands {
+    /// Adds files to myapp
+    #[command(about = "get a token from an oidc endpoint")]
+    Token(Token),
+    #[command(
+        about = "get config file from URL, use it for login and update the access token in local config files"
+    )]
+    Login(Login),
+}
+
+#[derive(Args)]
+struct Token {
+    #[arg(index = 1, help = "OIDC issue url")]
     url: String,
-    #[arg(short, long)]
+    #[arg(index = 2, help = "OIDC client_id")]
     client_id: String,
 }
+
+#[derive(Args)]
+struct Login {
+    #[arg(index = 1, help = "url to fetch config from")]
+    url: String,
+    #[arg(short, long, help = "whether to ask for confirmation or not")]
+    force: bool,
+}
+
 fn main() {
-    let args = Args::parse();
-    let token = device_code_flow(args.url, args.client_id).expect("couldn't get token");
-    println!("Token: {}", token.access_token());
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Token(args) => {
+            // just do device code flow if we have a client id and print the token
+            let token = do_device_flow(args.url.clone(), args.client_id.clone())
+                .expect("couldn't get token from endpoint");
+            println!("Access Token: \n{}", token.access_token());
+            if let Some(refresh_token) = token.refresh_token() {
+                println!("Refresh Token: \n{}", refresh_token);
+            }
+        }
+        Commands::Login(login) => {
+            let config = Config::download(login.url.clone()).expect("couldn't load config file");
+            let token = do_device_flow(config.url.clone(), config.client_id.clone())
+                .expect("couldn't get token from endpoint");
+            if login.force {
+                let (applied, skipped) = config.apply(token).expect("couldn't update config files");
+                if !skipped.is_empty() {
+                    println!(
+                        "Skipped these rules as they did not apply:\n\t{}",
+                        skipped.join("\n\t")
+                    )
+                }
+                if !applied.is_empty() {
+                    println!(
+                        "Applied these rules and updated files:\n\t{}",
+                        applied.join("\n\t")
+                    )
+                }
+                return;
+            }
+            // verify files to be updated with user
+            println!(
+                "Will update the following files:\n\t{}\nContinue?(Y/n)",
+                config
+                    .affected_paths()
+                    .expect("could not get affected paths")
+                    .iter()
+                    .map(|p| p.to_str().unwrap())
+                    .collect::<Vec<&str>>()
+                    .join("\n\t")
+            );
+            let mut input = String::new();
+            match std::io::stdin().read_line(&mut input) {
+                Ok(_) => {
+                    if input.trim().is_empty() || input.trim() == "Y" || input.trim() == "y" {
+                        let (applied, skipped) =
+                            config.apply(token).expect("couldn't update config files");
+                        if !skipped.is_empty() {
+                            println!(
+                                "Skipped these rules as they did not apply:\n\t{}",
+                                skipped.join("\n\t")
+                            )
+                        }
+                        if !applied.is_empty() {
+                            println!(
+                                "Applied these rules and updated files:\n\t{}",
+                                applied.join("\n\t")
+                            )
+                        }
+                    }
+                }
+                Err(error) => println!("error:{error}"),
+            }
+        }
+    }
+}
+
+fn do_device_flow(url: String, client_id: String) -> Result<ocli::OIDCTokenset> {
+    let data = start_device_code_flow(url.clone(), client_id.clone())
+        .expect("device code flow not successful");
+    println!(
+        "Please visit {} and authorize this application.",
+        data.verify_url_full
+    );
+    open::that(data.verify_url_full.clone())
+        .or_else(|_| {
+            println!(
+                "Couldn't open browser, please navigate to {}",
+                data.verify_url_full
+            );
+            std::io::Result::Ok(())
+        })
+        .unwrap();
+
+    finish_device_code_flow(data)
 }
